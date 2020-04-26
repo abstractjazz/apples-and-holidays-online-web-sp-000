852690a6a6b065a517ff206adc7c7b10b42f9d61
Evan Burton <evan@heyevan.com>
2020-04-26 01:59:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holidays|
 puts "#{season.to_s.capitalize!}:"
 holidays.each do |holiday, supplies|
-  puts "#{supplies}"
+  puts "#{supplies.join}"
   end
   end
 end
